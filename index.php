d56d010a79fdf44eeb288485f01973f93bef1c56
mostapha khalid <mustafaelkhokhy@gamil.com>
2025-05-25 10:32:34 +0300

    ecommerce project

--- index.php
+++ index.php
@@ -1,3 +1,47 @@
-<!-- git remote add origin https://github.com/elkhokh/E-Commerce-Electronics.git
-git branch -M master
-git push -u origin master -->
+<?php
+
+require_once "vendor/autoload.php";
+require_once "Config/database.php";
+
+// use database
+
+
+// database conn
+
+
+/******************  route system **********************/
+$page = isset($_GET['page']) ? $_GET['page'] : 'home';
+
+/****************** dashboard ********************** */
+
+if ($user_type) {
+      
+    include 'View/dashboard/layout/header.php';
+
+    $routes = [
+        'home' => 'View/dashboard/pages/home.php',
+        
+    ];
+
+    $file = isset($rout[$page]) ? $rout[$page] : './view/error/404.php';
+    include $file;
+
+    include 'View/dashboard/layout/footer.php';
+    
+}
+
+/****************** front ********************** */
+if ($user_type) {
+    include 'View/front/layout/header.php';
+
+    $routes = [
+        'home' => 'View/front/pages/home.php',
+    
+    ];
+
+    $file = $routes[$page] ?? 'View/Error/404.php';
+    include $file;
+
+    include 'View/front/layout/footer.php';
+    
+}
