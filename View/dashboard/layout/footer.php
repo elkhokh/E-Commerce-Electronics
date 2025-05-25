3605e2bd3a4c91b3ab8a1dba753d9a9181c4a408
mostapha khalid <mustafaelkhokhy@gamil.com>
2025-05-25 07:05:21 +0300

    ecommerce project

---
+++ View/dashboard/layout/footer.php
@@ -0,0 +1,17 @@
+			<footer class="main-footer">
+				
+				<strong>Copyright &copy; 2014-2022 AmazingShop All rights reserved.
+			</footer>
+			
+		</div>
+		<!-- ./wrapper -->
+		<!-- jQuery -->
+		<script src="public/assets/dashboard/plugins/jquery/jquery.min.js"></script>
+		<!-- Bootstrap 4 -->
+		<script src="public/assets/dashboard/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+		<!-- AdminLTE App -->
+		<script src="public/assets/dashboard/js/adminlte.min.js"></script>
+		<!-- AdminLTE for demo purposes -->
+		<script src="public/assets/dashboard/js/demo.js"></script>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,3 @@
+<!-- git remote add origin https://github.com/elkhokh/E-Commerce-Electronics.git
+git branch -M master
+git push -u origin master -->
